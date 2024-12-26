8f19ab9d28d14b5b25898fb2f1fa96c2eab3f7cf
Ianropke <ianropke@gmail.com>
2024-12-26 11:38:35 +0100

    Update IntroScreen.js

--- src/   components/IntroScreen.js
+++ src/   components/IntroScreen.js
@@ -1,1 +1,20 @@
+import React from "react";
+
+const IntroScreen = ({ onSelectLevel }) => {
+  return (
+    <div className="intro-screen">
+      <h1 className="game-title">Funky Rhythm</h1>
+      <p>Select a Level:</p>
+      <div className="level-buttons">
+        <button onClick={() => onSelectLevel("November")} className="level-button">
+          November
+        </button>
+        {/* Add more levels here */}
+        <button disabled className="level-button">Coming Soon...</button>
+      </div>
+    </div>
+  );
+};
+
+export default IntroScreen;
 
