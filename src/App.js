58fcc3c7f43ce860a7dd418829d9a5b4133917d4
Ianropke <ianropke@gmail.com>
2024-12-26 11:38:54 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
+import React, { useState } from "react";
+import IntroScreen from "./components/IntroScreen";
+import Game from "./components/Game";
+
+function App() {
+  const [level, setLevel] = useState(null);
+
+  const handleLevelSelect = (selectedLevel) => {
+    setLevel(selectedLevel); // Set the selected level (e.g., "November")
+  };
+
+  return (
+    <div>
+      {level === null ? (
+        <IntroScreen onSelectLevel={handleLevelSelect} />
+      ) : (
+        <Game level={level} />
+      )}
+    </div>
+  );
+}
+
+export default App;
 
