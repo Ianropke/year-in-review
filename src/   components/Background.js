0a4bcbf339a2c67512603c022d8e1a97fac698c6
Ianropke <ianropke@gmail.com>
2024-12-26 11:57:58 +0100

    Update Background.js

--- src/   components/Background.js
+++ src/   components/Background.js
@@ -1,1 +1,13 @@
+import React from "react";
+
+const Background = () => (
+  <div className="background">
+    <div className="stage">
+      <h1>Jamiroquai Rhythm Game</h1>
+    </div>
+    <div className="crowd" />
+  </div>
+);
+
+export default Background;
 
