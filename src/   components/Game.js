a5257915ea5fff66eb06aa23a768e996dd9008cb
Ianropke <ianropke@gmail.com>
2024-12-26 11:43:57 +0100

    Update Game.js

--- src/   components/Game.js
+++ src/   components/Game.js
@@ -1,1 +1,17 @@
-
+useEffect(() => {
+  fetch('/cards.json')
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Cards file not found");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      setCards(data);
+      // Add other logic here
+    })
+    .catch((err) => {
+      console.error(err.message);
+      setCards([]); // Use an empty array or fallback data
+    });
+}, []);
